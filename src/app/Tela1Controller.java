2e38c2a521c11fed6d80e43933a065f9e267b6f2
Bruno Tardin <brunotardin20@gmail.com>
2019-03-20 21:18:40 -0300

    Criadas as telas 1 e 2

--- src/app/Tela1Controller.java
+++ src/app/Tela1Controller.java
@@ -7,10 +7,9 @@ package app;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 /**
  *
@@ -19,12 +18,11 @@ import javafx.scene.control.Label;
 public class Tela1Controller implements Initializable {
     
     @FXML
-    private Label label;
+    private Button btnChamarTela2;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void onBtnChamarTela2Action(){
+        
     }
     
     @Override
